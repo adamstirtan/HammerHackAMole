dd2a311b4921ffabd613d57e94faebd6f977fa37
Adam Stirtan <adamstirtan@gmail.com>
2024-11-05 15:35:05 -0500

    Implements basic mulitplayer game loop

---
+++ src/webapp/src/components/Game.vue
@@ -0,0 +1,57 @@
+<script setup>
+  import { ref, onMounted } from 'vue';
+  import * as signalR from '@microsoft/signalr';
+
+  const score = ref(0);
+  const moles = ref(Array(9).fill(false));
+  const connection = new signalR.HubConnectionBuilder()
+    .withUrl('http://localhost:5107/game-hub')
+    .build();
+
+  onMounted(async () => {
+    try {
+      await connection.start();
+
+      connection.on('UpdateScore', (newScore) => {
+        score.value = newScore;
+      });
+
+      startGame();
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  const startGame = () => {
+    setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * moles.value.length);
+      moles.value = moles.value.map((_, index) => index === randomIndex);
+    }, 1000);
+  };
+
+  const whackMole = async (index) => {
+    if (moles.value[index]) {
+      try {
+        await connection.invoke('WhackMole');
+        moles.value[index] = false;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+</script>
+
+<template>
+  <div class="container">
+    <div class="score">Score: {{ score }}</div>
+    <div id="game">
+      <div v-for="(mole, index) in moles" :key="index" class="hole" @click="whackMole(index)">
+        <div v-if="mole" class="mole"></div>
+      </div>
+    </div>
+    <div class="join">
+      <span class="subtle">https://</span>bit.ly/3z5Q7ZC
+    </div>
+  </div>
+</template>
+
