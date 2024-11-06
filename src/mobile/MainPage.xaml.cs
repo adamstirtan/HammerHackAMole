efb39a0c74335a75b3bfffbcfaf994746ebef142
Adam Stirtan <adamstirtan@gmail.com>
2024-11-06 14:10:57 -0500

    Matches visual style

--- src/mobile/MainPage.xaml.cs
+++ src/mobile/MainPage.xaml.cs
@@ -1,18 +1,59 @@
-﻿namespace mobile;
+﻿using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace mobile;
 
 public partial class MainPage : ContentPage
 {
-    private int _count = 0;
+    private readonly Random _random = new();
+    private readonly ImageButton[] _moleButtons;
+    
+    private Timer? _gameTimer;
+    private int _score = 0;
 
     public MainPage()
     {
         InitializeComponent();
+        
+        _moleButtons =
+        [
+            MoleButton0, MoleButton1, MoleButton2, MoleButton3, MoleButton4, MoleButton5, MoleButton6, MoleButton7, MoleButton8
+        ];
+        
+        StartGame();
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
+    private void StartGame()
     {
-        _count++;
+        _gameTimer = new Timer(1000);
+        _gameTimer.Elapsed += OnGameTimerElapsed;
+        _gameTimer.Start();
+    }
 
-        CounterBtn.Text = _count == 1 ? $"Clicked {_count} time" : $"Clicked {_count} times";
+    private void OnGameTimerElapsed(object? sender, ElapsedEventArgs e)
+    {
+        Dispatcher.Dispatch(() =>
+        {
+            foreach (ImageButton button in _moleButtons)
+            {
+                button.Source = null;
+            }
+
+            int randomIndex = _random.Next(_moleButtons.Length);
+            _moleButtons[randomIndex].Source = "mole.png";
+        });
+    }
+
+    private void OnMoleClicked(object sender, EventArgs e)
+    {
+        ImageButton? clickedButton = sender as ImageButton;
+        
+        if (clickedButton?.Source != null)
+        {
+            clickedButton.Source = null;
+            
+            _score++;
+            ScoreLabel.Text = $"Score: {_score}";
+        }
     }
 }
